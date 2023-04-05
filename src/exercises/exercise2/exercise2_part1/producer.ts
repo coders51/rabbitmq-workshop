6b8f9ae4744386c558e6a172873f4414b833e17a
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-05 09:57:44 +0200

    esercizio2 parte1

---
+++ src/exercises/exercise2/exercise2_part1/producer.ts
@@ -0,0 +1,36 @@
+import { connect, ConsumeMessage } from "amqplib";
+import { random } from "lodash";
+
+random();
+export function yyz(m: ConsumeMessage) {
+  console.log(m);
+}
+
+export function wait(timeout: number) {
+  return new Promise((res, _) => {
+    return setTimeout(res, timeout);
+  });
+}
+
+async function main() {
+  const connection = await connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  const exchangeName = "fanoutExchange";
+  await channel.assertExchange(exchangeName, "fanout");
+  let cont = 0;
+  while (true) {
+    if (cont == 3) cont = 1;
+    else cont++;
+    const msg = `messaggio` + cont;
+    const rk = cont;
+    channel.publish(exchangeName, rk.toString(), Buffer.from(msg));
+    console.log("Sent: ", msg, "with key = " + rk);
+    await wait(1000);
+  }
+  await connection.close();
+}
+
+main().then(
+  () => console.log("OK!"),
+  err => console.log("Error: ", err)
+);
