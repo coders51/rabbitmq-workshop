cd8d366dc5f620904086f4f39f052d8d0fc14078
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-05 08:32:49 +0200

    part1

---
+++ src/exercises/exercise1/exercise1_part1/consumer.ts
@@ -0,0 +1,34 @@
+import { connect, ConsumeMessage } from "amqplib";
+import { random } from "lodash";
+
+random();
+export function yyz(m: ConsumeMessage) {
+  console.log(m);
+}
+
+export function wait(timeout: number) {
+  return new Promise((res, _) => {
+    return setTimeout(res, timeout);
+  });
+}
+
+async function main() {
+  const connection = await connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  const exchangeName = "exchangeDirect";
+  await channel.assertExchange(exchangeName, "direct");
+  const { queue } = await channel.assertQueue("", {
+    autoDelete: true,
+    durable: false,
+  });
+  console.log(
+    " [*] Waiting for messages in " + queue + " - To exit press CTRL+C"
+  );
+  channel.bindQueue(queue, "exchangeDirect", "");
+  channel.consume(queue, msg => console.log(msg?.content.toString()));
+}
+
+main().then(
+  () => console.log("Ok"),
+  err => console.log("Error: ", err)
+);
