99e1b37725e89f32bc41eac77160e80dbe263237
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-05 15:59:19 +0200

    fix

--- src/exercises/direct/consumer.ts
+++ src/exercises/direct/consumer.ts
@@ -11,23 +11,23 @@ export function wait(timeout: number) {
 }
 
 async function main() {
-  //Using yargs. To set the the queue pass as argument --queue <queue_number>
-  //<queue_number>
   const argv = yargs(process.argv.slice(2))
     .options({
-      queue: { type: "string", default: "1" },
+      queue: { type: "string", default: "q1" },
+      rk: { type: "string", default: "rk1" },
     })
     .parseSync();
-  console.log(argv.queue);
+  console.log("consuming on queue: ", argv.queue);
+
   const connection = await connect("amqp://localhost");
   const channel = await connection.createChannel();
   const exchangeName = "exchangeDirect";
   await channel.assertExchange(exchangeName, "direct");
-  const { queue } = await channel.assertQueue(`queue ${argv.queue}`, {
+  const { queue } = await channel.assertQueue(`${argv.queue}`, {
     autoDelete: false,
     durable: true,
   });
-  const rk = `rk${argv.queue}`;
+  const rk = argv.rk;
   console.log(` [*] Waiting for messages in ${queue} - To exit press CTRL+C`);
   channel.bindQueue(queue, "exchangeDirect", rk);
   channel.consume(queue, msg => console.log(msg?.content.toString()));
