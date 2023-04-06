ee6cfc00b691aba25dd66abb7a4b87105c09e342
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-06 14:48:18 +0200

    correct await & changed exchange

--- src/exercises/order/order_consumer.ts
+++ src/exercises/order/order_consumer.ts
@@ -10,29 +10,32 @@ export function wait(timeout: number) {
   });
 }
 async function main() {
-    const argv = yargs(process.argv.slice(2))
+  const argv = yargs(process.argv.slice(2))
     .options({
-      queue: {type: "string", default: "q1"},
-      rk: { type: "string", default: "" },
+      queue: { type: "string", default: "q1" },
+      rk: { type: "string", require: true },
+      exchange: { type: "string", default: "orderExchange" },
     })
     .parseSync();
   const connection = await connect("amqp://localhost");
   const channel = await connection.createChannel();
-  const exchangeName = "orderEx";
-  await channel.assertExchange(exchangeName, "fanout");
-  const { queue } = await channel.assertQueue(argv.queue, { autoDelete: false, durable: true });
+  const exchangeName = argv.exchange;
+  await channel.assertExchange(exchangeName, "topic");
+  const { queue } = await channel.assertQueue(argv.queue, {
+    autoDelete: false,
+    durable: true,
+  });
   console.log(" [*] Waiting for orders in: " + queue + " - To exit press CTRL+C");
   channel.bindQueue(queue, exchangeName, argv.rk);
-  channel.consume(queue, (msg) => {
-    if(!msg){
-        return;
+  channel.consume(queue, async (msg) => {
+    if (!msg) {
+      return;
     }
-    const order = JSON.parse(msg.content.toString()); 
-        console.log(`Order: ${order.orderId} - total: ${order.total}`); 
-        wait(1500);
-        channel.ack(msg);
-});
-  
+    const order = JSON.parse(msg.content.toString());
+    await wait(1500);
+    console.log(`Order: ${order.orderId} - total: ${order.total}`);
+    channel.ack(msg);
+  });
 }
 
 main().then(
