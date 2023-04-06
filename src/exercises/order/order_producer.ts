c6c7f4f1be0ad58b6d89e909fe671ce4a5b61146
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-06 10:05:47 +0200

    order producer & order consumer

---
+++ src/exercises/order/order_producer.ts
@@ -0,0 +1,41 @@
+import { connect } from "amqplib";
+import { random } from "lodash";
+import yargs from "yargs";
+
+random();
+
+export function wait(timeout: number) {
+  return new Promise((res, _) => {
+    return setTimeout(res, timeout);
+  });
+}
+
+async function main() {
+  const argv = yargs(process.argv.slice(2))
+    .options({
+      order: { type: "string", default: "orderName" },
+    })
+    .parseSync();
+  const connection = await connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  const exchangeName = "orderEx";
+  await channel.assertExchange(exchangeName, "fanout");
+  let count = 0;
+  while (true) {
+    let orderId = random(1000, 10000)
+    let total = random(1,50)
+    const message = { type: argv.order, orderId: orderId, total: total };
+    const jsonMessage = JSON.stringify(message);
+    const rk = `rk${count + 1}`;
+    channel.publish(exchangeName, rk.toString(), Buffer.from(jsonMessage));
+    console.log("Sent order: " + argv.order + ", id: " + orderId +  ", rk: " + rk);
+    await wait(1000);
+    count = (count + 1) % 3;
+  }
+  await connection.close();
+}
+
+main().then(
+  () => console.log("OK!"),
+  err => console.log("Error: ", err)
+);
