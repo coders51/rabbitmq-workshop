806012b5f35f4b9a83b5f8aecefacb7e9dd29e87
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-06 18:06:24 +0200

    fix args

--- src/exercises/consumer_error/consumer_error.ts
+++ src/exercises/consumer_error/consumer_error.ts
@@ -12,34 +12,34 @@ export function wait(timeout: number) {
 async function main() {
     const argv = yargs(process.argv.slice(2))
     .options({
-      queue: {type: "string", default: "q1"},
-      rk: { type: "string", default: "" },
+      queue: { type: "string", default: "q1" },
+      rk: { type: "string", require: true },
+      exchange: { type: "string", default: "orderErrExchange" },
     })
     .parseSync();
   const connection = await connect("amqp://localhost");
   const channel = await connection.createChannel();
-  const exchangeName = "orderErrEx";
-  await channel.assertExchange(exchangeName, "fanout");
+  const exchangeName = argv.exchange;
+  await channel.assertExchange(exchangeName, "topic");
   const { queue } = await channel.assertQueue(argv.queue, { autoDelete: false, durable: true });
   console.log(" [*] Waiting for orders in: " + queue + " - To exit press CTRL+C");
-  channel.bindQueue(queue, exchangeName, argv.rk);
-  await channel.consume(queue, (msg) => {
+  channel.bindQueue(queue, argv.exchange, argv.rk);
+  channel.consume(queue, async msg => {
     if(!msg){
         return;
     }
     const order = JSON.parse(msg.content.toString()); 
     try{
-        if(order.total >= 80){
-            console.log(`Rejected Order: ${order.orderId} due to total > 80 **requeued**`); 
-            channel.nack(msg);
+        if(order.total >= 1000000000){
             throw new Error();
         }
+        await wait(1500);
         console.log(`Order: ${order.orderId} - total: ${order.total}`);
+        channel.ack(msg);
     } catch (error) {
-        console.log(">>> Error", error);
-    } 
-    wait(1500);
-    channel.ack(msg);
+      await wait(1500);
+      console.log(`Rejected Order: ${order.orderId} due to total > 80`); 
+    }
 });
   
 }
