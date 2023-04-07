817cdc4697f7530ee4efc1a0f9eb55c748529eaa
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-07 12:02:51 +0200

    retry & errors

--- src/exercises/consumer_error/order_producer.ts
+++ src/exercises/consumer_error/order_producer.ts
@@ -1,5 +1,5 @@
 import { connect } from "amqplib";
-import { random } from "lodash";
+import { random, } from "lodash";
 import yargs from "yargs";
 
 random();
@@ -14,11 +14,12 @@ async function main() {
   const argv = yargs(process.argv.slice(2))
     .options({
       order: { type: "string", default: "orderName" },
+      exchange: { type: "string", default: "orderErrExchange" },
     })
     .parseSync();
   const connection = await connect("amqp://localhost");
-  const channel = await connection.createChannel();
-  const exchangeName = "orderErrExchange";
+  const channel = await connection.createConfirmChannel();
+  const exchangeName = argv.exchange;
   await channel.assertExchange(exchangeName, "topic");
   while (true) {
     let orderId = random(1000, 10000)
@@ -26,9 +27,12 @@ async function main() {
     const message = { type: argv.order, orderId: orderId, total: total };
     const jsonMessage = JSON.stringify(message);
     const rk = argv.order;
-    channel.publish(exchangeName, rk, Buffer.from(jsonMessage));
+    channel.publish(exchangeName, rk, Buffer.from(jsonMessage), {
+      persistent: true,
+    });
     console.log("Sent order id: " + orderId + ", total: " + total + ", rk: " + rk );
     await wait(1000);
+    await channel.waitForConfirms();
   }
   await connection.close();
 }
