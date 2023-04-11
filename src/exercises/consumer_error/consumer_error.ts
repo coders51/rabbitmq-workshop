8195d7ae2aaa1ebddd657bf7a3fcec7e094f9c20
FedericoDiTosto <federico.ditosto@gmail.com>
2023-04-11 10:38:19 +0200

    quorum & redelivery

--- src/exercises/consumer_error/consumer_error.ts
+++ src/exercises/consumer_error/consumer_error.ts
@@ -21,26 +21,28 @@ async function main() {
   const channel = await connection.createConfirmChannel();
   const exchangeName = argv.exchange;
   await channel.assertExchange(exchangeName, "topic");
-  const { queue } = await channel.assertQueue(argv.queue, { autoDelete: false, durable: true,});
+  const { queue } = await channel.assertQueue(argv.queue, { autoDelete: false, durable: true, arguments:{"x-queue-type": "quorum"}});
   console.log(" [*] Waiting for orders in: " + queue + " - To exit press CTRL+C");
   channel.bindQueue(queue, argv.exchange, argv.rk);
-  let deliveryCount = 0;
   channel.consume(queue, async msg => {
     if(!msg){
         return;
     }
     const order = JSON.parse(msg.content.toString());  
     try{
-        if(order.total >= 80 && random(1, 10) % 2 === 0){
+      if(order.total >= 80){
+        channel.nack(msg)
+          return;
+      }
+      if(random(1, 10) % 2 === 0){
             throw new Error();
-        }
-        deliveryCount = 0;
+      }
         console.log(`Order: ${order.orderId} - total: ${order.total}`);
         channel.ack(msg);
     } catch (error) {
+        const deliveryCount = msg.properties.headers["x-delivery-count"] || 0;
         console.error(`Rejected Order: ${order.orderId} - redelivered: ${deliveryCount} - requeuing: ${deliveryCount < 10}`); 
-        channel.nack(msg, false, deliveryCount <= 10);
-        deliveryCount++;
+        channel.nack(msg, false, deliveryCount < 10);
     }
 });
   
